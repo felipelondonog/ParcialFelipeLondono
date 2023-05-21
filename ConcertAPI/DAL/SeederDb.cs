d6e3948ca9cb00574aea3b96700562c20b3d65f2
felip <felipe12091@hotmail.com>
2023-05-21 16:15:00 -0500

    Created ConcertDB_FelipeLondono database

--- ConcertAPI/DAL/SeederDb.cs
+++ ConcertAPI/DAL/SeederDb.cs
@@ -7,5 +7,24 @@
         {
             _context = context;
         }
+
+        public async Task SeederAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+            await PopulateTicketsAsync();
+
+            await _context.SaveChangesAsync();
+        }
+
+        private async Task PopulateTicketsAsync()
+        {
+            if (!_context.Tickets.Any())
+            {
+                for(int i = 0; i < 50000;  i++)
+                {
+                    _context.Tickets.Add(new Ticket { IsUsed = false });
+                }
+            }
+        }
     }
 }
--- ConcertAPI/Program.cs
+++ ConcertAPI/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using ConcertAPI.DAL;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +12,29 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<DataBaseContext>(o =>
+{
+    o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddTransient<SeederDb>();
+
+builder.Services.AddControllers().AddJsonOptions(x =>
+                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
 var app = builder.Build();
+
+SeederData();
+
+void SeederData()
+{
+    IServiceScopeFactory? scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+    using (IServiceScope? scope = scopedFactory.CreateScope())
+    {
+        SeederDb? service = scope.ServiceProvider.GetService<SeederDb>();
+        service.SeederAsync().Wait();
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -15,6 +42,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
